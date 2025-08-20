c2b3dbc015837a883bca2886e00dcc2bb563ce47
Pritam Bose <prit92bose@gmail.com>
2025-08-20 09:50:16 +0000

    WIP updating logic for flattening nested object helper function

--- kingfisher_demo_project/src/utils/helper.ts
+++ kingfisher_demo_project/src/utils/helper.ts
@@ -1,7 +1,7 @@
-type FlattenObjectTest = {
+type Flatten = {
     [key: string]: any
 }
-const testObj = {
+const testObj: Flatten = {
     a: 1,
     b: {
         c: 'hello',
@@ -14,31 +14,45 @@ const testObj = {
 
 
 }
-const result = {
+const result: Flatten = {
     a: '1',
     'b.c': 'hello',
     'b.d.e': true,
     'f.0.g': 'world'
 
 }
-export const flattenObjectHelper = (obj: any = testObj) => {
-    let flattenObj: any = {}
-    const nativeTypes = ['number', 'string', 'boolean']
-    Object.keys(obj).forEach((key) => {
-        console.log('**Value Value**', obj)
-        console.log('**Key Value**', key)
-        if (obj[key] !== null) {
-            if (nativeTypes.includes(typeof obj[key])) {
-                flattenObj[key] = obj[key]
+export const flattenObjectHelper = (obj: Flatten = testObj): Flatten => {
+    let flattenObj: Flatten = {}
+    for (const key in obj) {
+        if (isNotNullObject(obj[key])) {
+            const tempObject = flattenObjectHelper(obj[key])
+            for (const innerKey in tempObject) {
+                const tempInnerObject = checkNativeTypes(tempObject[innerKey] as Flatten)
+                if (tempInnerObject) {
+                    flattenObj[`${key}.${innerKey}`] = tempInnerObject
+                }
             }
         }
-        if (typeof obj[key] === 'object') {
-            console.log('Test Log')
-
+        else {
+            const tempValue = checkNativeTypes(obj[key])
+            if (tempValue) {
+                flattenObj[key] = tempValue
+            }
         }
-
-    })
+    }
     console.log('**Expected Value**', result)
     console.log('***FINAL VALUE**', flattenObj)
+    return flattenObj
 
+}
+
+const checkNativeTypes = (tempObj: Flatten): Flatten | null => {
+    const nativeTypes = ['number', 'string', 'boolean']
+    if (tempObj !== null && nativeTypes.includes(typeof tempObj))
+        return tempObj
+    else return null
+}
+
+const isNotNullObject = (obj: Flatten): boolean => {
+    return obj !== null && typeof obj === 'object'
 }
